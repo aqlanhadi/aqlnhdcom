f92d51452c7f82b06b314e246527c620f8ec555e
Aqlan Nor Azman <aqlanhadi@gmail.com>
2020-06-07 16:07:28 +0800

    site layout completed for desktop

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
-export default Footer = () => (
+const Footer = () => (
     <footer>
-        <div class="footer-wrapper">
-            <ul>
-                <li>Instagram.</li>
-                <li>Twitter.</li>
-            </ul>
+        <div className="container">
+            <div className="footer-wrapper">
+                <div className="left">
+                    {/* <div className="big">12</div> */}
+                    <div className="small"></div>
+                </div>
+                <div className="socials">
+                    <nav>
+
+                        <Link to="/">Ig.</Link>
+                        <Link to="/">Yt.</Link>
+                    </nav>
+                </div>
+            </div>
         </div>
+        
     </footer>
 )
+
+export default Footer
--- src/components/index-blurb.js
+++ src/components/index-blurb.js
@@ -1,54 +1,94 @@
 import React, { useRef, useEffect } from 'react'
+import { useIntersection } from 'react-use'
+import gsap from 'gsap'
 //import { useStaticQuery, graphql } from 'gatsby'
 //import Img from 'gatsby-image'
 
 const IndexBlurb = () => {
-    // const data = useStaticQuery(graphql`
-    //     query {
-    //         layerOne: file(relativePath: { eq: "1.png" }) {
-    //             ...fluidImage
-    //         },
-    //         layerTwo: file(relativePath: { eq: "2.png" }) {
-    //             ...sharpestImage
-    //         }
-    //         layerThree: file(relativePath: { eq: "3.png" }) {
-    //             ...fluidImage
-    //         }
-    //         layerFour: file(relativePath: { eq: "4.png" }) {
-    //             ...fluidImage
-    //         }
-    //     }
-    // `)
+
+    const section = useRef()
+
+    const intersection = useIntersection(section, {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.7
+    })
+
+    const fadeOut = element => {
+        console.log(element, " out.")
+        gsap.to(element, 1, {
+            opacity: 0,
+            ease: 'power4.out',
+            stagger: {
+                amount: -0.3
+            }
+        })
+    }
+
+    const fadeIn = element => {
+        console.log(element, " in.")
+        gsap.to(element, 1, {
+            opacity: 1,
+            ease: 'power4.out',
+            stagger: {
+                amount: 0.3
+            }
+        })
+    }
+    
+
+    useEffect(() => {
+        console.log(intersection)
+        intersection && intersection.isIntersecting
+        ? fadeIn(".fade")
+        : fadeOut(".fade")
+        
+    })
+
+        
+
+    
 
     return (
         <div className="about-blurb">
             <div className="container">
-                <div className="image-boxes">
-                    <div className="image-box-wrapper one">
-                        <img alt=""
-                        src="https://images.unsplash.com/photo-1550422086-b9589ec2d371?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80"
-                        />
-                        <h4>WOODS</h4>
+                <div ref={section} className="inner-container">
+                    <div className="image-container">
+                        <div className="row">
+                            <div className="image-box-wrapper">
+                                <img class="fade" alt=""
+                                src="https://images.unsplash.com/photo-1550422086-b9589ec2d371?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80"
+                                />
+                            <h4>Woods.</h4>
+                            </div>
+                            <div className="image-box-wrapper">
+                                <img class="fade" alt=""
+                                src="https://images.unsplash.com/photo-1550433251-d1a8ff40ff05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+                                />
+                                <h4>City.</h4>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="image-box-wrapper">
+                                <img class="fade" alt=""
+                                src="https://images.unsplash.com/photo-1550364387-ffbad4f8e9b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+                                />
+                                <h4>Mountains.</h4>
+                            </div>
+                            <div className="image-box-wrapper">
+                                <img class="fade" alt=""
+                                src="https://images.unsplash.com/photo-1550183203-8d3ca82400c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+                                />
+                                <h4>History.</h4>
+                            </div>
+                        </div>
                     </div>
-                    <div className="image-box-wrapper two">
-                        <img alt=""
-                        src="https://images.unsplash.com/photo-1550433251-d1a8ff40ff05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-                        />
-                        <h4>CITY</h4>
-                    </div>
-                    <div className="image-box-wrapper three">
-                        <img alt=""
-                        src="https://images.unsplash.com/photo-1550364387-ffbad4f8e9b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-                        />
-                        <h4>MOUNTANS</h4>
-                    </div>
-                    <div className="image-box-wrapper four">
-                        <img alt=""
-                        src="https://images.unsplash.com/photo-1550183203-8d3ca82400c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-                        />
-                        <h4>HISTORY</h4>
+                    <div className="text-blurb">
+                        <h1>Paragraph.</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
+
             </div>
         </div>
     )
